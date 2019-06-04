180a1316b250a6e5e16188b34ed28d99b1d6beba
Tobias Urban <turban2009@live.de>
2019-06-04 13:31:42 +0200

    Complete re-design

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use("/js",express.static("js"));
 app.use("/css",express.static("css"));
-app.use("/src",express.static("src"));
+app.use("/assets",express.static("assets"));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
