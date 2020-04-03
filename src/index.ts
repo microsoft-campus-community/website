50756a398c1361bafc8579a45dc9c69f4531caf3
Urmade <tobias-urban@outlook.com>
2020-04-03 10:09:09 +0200

    Refactored website hosting to node-server

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import express from "express";
+
+const server = express();
+const assets = path.join(__dirname, "..", "static");
+
+server.use(express.static(assets));
+
+server.get("/",(req,res) => {
+	res.sendFile(getStaticFile("index.html"));
+});
+server.get("/digitalLearning",(req,res) => {
+	res.sendFile(getStaticFile("DigitalLearning.html"));
+});
+server.get("/insideMicrosoft",(req,res) => {
+	res.sendFile(getStaticFile("insideMicrosoft.html"));
+});
+server.get("/mlhlocalhackday",(req,res) => {
+	res.sendFile(getStaticFile("mlhLocalHackDay.html"));
+});
+server.get("/eventcal",(req,res) => {
+	res.sendFile(getStaticFile("eventcal.html"));
+});
+
+
+server.get("/legal",(req,res) => {
+	res.sendFile(getStaticFile("legal.html"));
+});
+server.get("/privacy",(req,res) => {
+	res.sendFile(getStaticFile("privacy.html"));
+});
+
+
+function getStaticFile(fileName:string):string {
+	return path.join(assets,fileName);
+}
+
+server.listen(process.env.PORT || 8080);
